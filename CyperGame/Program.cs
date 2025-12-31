b5226f5b82b6f3e2ac3a6522f56cb12945562161
Hüseyin Ardıl Karasungur <ardilkarasungur12@gmail.com>
2025-12-31 09:45:32 +0300

    CyperGame Created
    CyperGame Created

---
+++ CyperGame/Program.cs
@@ -0,0 +1,35 @@
+using CyperGame.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<IGameService, GameService>();
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseSession();
+app.UseAuthorization();
+
+app.MapStaticAssets();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Game}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+app.Run();
